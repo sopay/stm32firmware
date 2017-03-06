a6d78ac9ae5d0981cd7249ff41a3fe2b07f58a51
sopay <sopay@users.noreply.github.com>
2017-03-06 20:09:03 +0100

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -240,7 +240,14 @@ void MainTask(void) {
   #if GUI_SUPPORT_MEMDEV
     WM_SetCreateFlags(WM_CF_MEMDEV);
   #endif
-
+	
+  GPIO_InitDef.GPIO_Pin = GPIO_Pin_13;
+  GPIO_InitDef.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitDef.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitDef.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_InitDef.GPIO_Speed = GPIO_Speed_100MHz;
+  //Initialize pins
+  GPIO_Init(GPIOG, &GPIO_InitDef);
 
   while (1) {
     GUI_Delay(100);
